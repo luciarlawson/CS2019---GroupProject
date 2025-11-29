47ee69e12eca4a410a8f1700c448070a439e039e
luciarlawson <l.lawson.24@abdn.ac.uk>
2025-11-29 23:23:45 +0000

    Foreign keys
    IT WORKS!!!!! Foreign keys and primary keys all working

--- Task_8.sql
+++ Task_8.sql
@@ -1,43 +1,38 @@
 
-CREATE TABLE `Attendance` (
-  `FlightID` BIGINT(25) NOT NULL,
-  `PartyID` VARCHAR(25) NOT NULL,
-  `AttendanceStatus` ENUM('Booked','Completed','Cancelled') NOT NULL
-  
-  );
-
-
-CREATE TABLE `Flight` (
-  `FlightID` BIGINT(25) UNSIGNED NOT NULL AUTO_INCREMENT ,
-  `Duration` ENUM('30mins','1hour','2hours','12hours') NOT NULL ,
-  `Date` DATE NOT NULL ,
-  `Time` TIME NOT NULL ,
-  `Route` ENUM('Costal','Sunset','Overseas','Night-time') NOT NULL ,
-  `FlightStatus` ENUM('Completed','In-progress','Cancelled') NOT NULL ,
+  CREATE TABLE `Flight` (
+  `FlightID` VARCHAR(25) NOT NULL,
+  `Duration` ENUM('30mins','1hour','2hours','12hours') NOT NULL,
+  `Date` DATE NOT NULL,
+  `Time` TIME NOT NULL,
+  `Route` ENUM('Coastal','Sunset','Overseas','Night-time') NOT NULL,
+  `FlightStatus` ENUM('Completed','In-progress','Cancelled') NOT NULL,
 
   PRIMARY KEY (`FlightID`)
-
-  );
-
-
-CREATE TABLE `PleasureFlight` (
-  `FlightID` BIGINT(25) UNSIGNED NOT NULL AUTO_INCREMENT ,
-  `PartyID` VARCHAR(25) NOT NULL,
-
-  PRIMARY KEY (`FlightID`)
-  
-  );
+);
 
 
-CREATE TABLE `Crew` (
-  `PilotID` BIGINT(25) NOT NULL,
-  `FLightID` BIGINT(25) NOT NULL
+CREATE TABLE `Party` (
+  `PartyID` VARCHAR(25) NOT NULL ,
+  `NoOfMembers` INT(1) NOT NULL,
+  `ContactNo` VARCHAR(35) NOT NULL,
+
+  PRIMARY KEY (`PartyID`)
+
+);
+
+
+CREATE TABLE `Trainee_Officer` (
+  `TraineeID` VARCHAR(25) NOT NULL ,
+  `Fname` VARCHAR(35) NOT NULL,
+  `Lname` VARCHAR(35) NOT NULL,
+
+  PRIMARY KEY (`TraineeID`)
 
 );
 
 
 CREATE TABLE `Pilot` (
-  `PilotID` BIGINT(25) UNSIGNED NOT NULL AUTO_INCREMENT, 
+  `PilotID` VARCHAR(25) NOT NULL , 
   `Fname` VARCHAR(35) NOT NULL,
   `Lname` VARCHAR(35) NOT NULL,
   `Role` ENUM('Captain','Chief Officer','Officer','') NOT NULL,
@@ -47,54 +42,89 @@ CREATE TABLE `Pilot` (
   );
 
 
-CREATE TABLE `Party` (
-  `PartyID` BIGINT(25) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `NoOfMembers` INT(1) NOT NULL,
-  `ContactNo` VARCHAR(35) NOT NULL,
-
-  PRIMARY KEY (`PartyID`)
-
-);
-
-
 CREATE TABLE `Instructor` (
-  `InstructorID` BIGINT(25) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `InstructorID` VARCHAR(25) NOT NULL ,
 
   PRIMARY KEY (`InstructorID`)
 
 );
 
 
+CREATE TABLE `Attendance` (
+  `FlightID` VARCHAR(25) NOT NULL,
+  `PartyID` VARCHAR(25) NOT NULL,
+  `AttendanceStatus` ENUM('Booked','Completed','Cancelled') NOT NULL,
+
+  CONSTRAINT `fk_attendance_flight`
+    FOREIGN KEY (`FlightID`) REFERENCES `Flight` (`FlightID`)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+
+  CONSTRAINT `fk_attendance_party`
+    FOREIGN KEY (`PartyID`) REFERENCES `Party` (`PartyID`)
+    ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+
+CREATE TABLE `PleasureFlight` (
+  `FlightID` VARCHAR(25) NOT NULL ,
+  `PartyID` VARCHAR(25) NOT NULL,
+
+  PRIMARY KEY (`FlightID`),
+  
+  CONSTRAINT `fk_pleasure_flight`
+    FOREIGN KEY (`FlightID`) REFERENCES `Flight` (`FlightID`)
+    ON DELETE RESTRICT ON UPDATE CASCADE
+  );
+
+
+CREATE TABLE `Crew` (
+  `PilotID` VARCHAR(25) NOT NULL,
+  `FLightID` VARCHAR(25) NOT NULL,
+
+  CONSTRAINT `fk_crew_flight`
+    FOREIGN KEY (`FlightID`) REFERENCES `Flight` (`FlightID`)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+
+  CONSTRAINT `fk_crew_pilot`
+    FOREIGN KEY (`PilotID`) REFERENCES `Pilot` (`PilotID`)
+    ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+
 CREATE TABLE `Passenger` (
-  `PassengerID` BIGINT(25) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `PartyID` BIGINT(25) NOT NULL,
+  `PassengerID` VARCHAR(25) NOT NULL ,
+  `PartyID` VARCHAR(25) NOT NULL,
   `Fname` VARCHAR(35) NOT NULL,
   `Lname` VARCHAR(35) NOT NULL,
   `Adult` BOOLEAN, 
   `PhoneNo` VARCHAR(11) NOT NULL,
 
-  PRIMARY KEY (`PassengerID`)
+  PRIMARY KEY (`PassengerID`),
 
+  CONSTRAINT `fk_passenger_party`
+    FOREIGN KEY (`PartyID`) REFERENCES `Party` (`PartyID`)
+    ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 
-CREATE TABLE `Training Flight` (
-  `FlightID` BIGINT(25) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `TraineeID` BIGINT(25) NOT NULL,
-  `Instructor` BIGINT(25) NOT NULL,
+CREATE TABLE `Training_Flight` (
+  `FlightID` VARCHAR(25) NOT NULL ,
+  `TraineeID` VARCHAR(25) NOT NULL,
+  `InstructorID` VARCHAR(25) NOT NULL,
 
-  PRIMARY KEY (`FlightID`)
+  PRIMARY KEY (`FlightID`),
+
+  CONSTRAINT `fk_training_flight`
+    FOREIGN KEY (`FlightID`) REFERENCES `Flight` (`FlightID`)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+
+  CONSTRAINT `fk_training_trainee`
+    FOREIGN KEY (`TraineeID`) REFERENCES `Trainee_Officer` (`TraineeID`)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+
+  CONSTRAINT `fk_training_instructor`
+    FOREIGN KEY (`InstructorID`) REFERENCES `Instructor` (`InstructorID`)
+    ON DELETE RESTRICT ON UPDATE CASCADE
 
 );
-
-
-CREATE TABLE `Trainee Offier` (
-  `TraineeID` BIGINT(25) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `Fname` VARCHAR(35) NOT NULL,
-  `Lname` VARCHAR(35) NOT NULL,
-
-  PRIMARY KEY (`TraineeID`)
-
-);
-
 
--- TestFile.sql
+++ TestFile.sql
@@ -16,3 +16,26 @@ CREATE TABLE `u02ll24_Project`.`Flight` (
   PRIMARY KEY (`FlightID`)
 
   ) ENGINE = InnoDB;
+
+
+  CREATE TABLE `Flight` (
+  `FlightID` VARCHAR(25) NOT NULL,
+  `Duration` ENUM('30mins','1hour','2hours','12hours') NOT NULL,
+  `Date` DATE NOT NULL,
+  `Time` TIME NOT NULL,
+  `Route` ENUM('Costal','Sunset','Overseas','Night-time') NOT NULL,
+  `FlightStatus` ENUM('Completed','In-progress','Cancelled') NOT NULL,
+  PRIMARY KEY (`FlightID`)
+);
+
+
+CREATE TABLE `Attendance` (
+  `FlightID` VARCHAR(25) NOT NULL,
+  `PartyID` VARCHAR(25) NOT NULL,
+  `AttendanceStatus` ENUM('Booked','Completed','Cancelled') NOT NULL,
+
+  CONSTRAINT `fk_attendance_flight`
+    FOREIGN KEY (`FlightID`) REFERENCES `Flight` (`FlightID`)
+    ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
